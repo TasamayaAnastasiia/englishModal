104d291c1f92214f22ea21e893a0208463ca70e7
totsamii-alex <144161343+TasamayaAnastasiia@users.noreply.github.com>
2024-03-22 20:48:22 +0100

    aaa

--- src/modal.js
+++ src/modal.js
@@ -1,6 +1,5 @@
 import axios from "axios";
-import close from './sprite.svg#close-icon';
-const buttonClick = document.querySelector(".but");
+const buttonClick = document.querySelector(".btn-connect");
 const modalWindow = document.querySelector(".mdl_box");
 const contentForm = document.querySelector(".mdl_content")
 const butClose = document.querySelector(".close");
@@ -10,88 +9,8 @@ const inputUserPhone = document.querySelector(".phone");
 const formUser = document.querySelector(".forma_for_connect");
 const labelText = document.querySelector("label-inpt");
 const token = "6077606622:AAG6g12itzLvnsQfazmk9-oBfkHb1kflQYk";
-let idCzat;
+let idCzat = -1002080915692;
 
-//get token bot//
-axios.get(`https://api.telegram.org/bot${token}/getUpdates`)
-    .then(response => { 
-        idCzat = response.data.result[response.data.result.length - 1].message.chat.id;
-    })
-    .catch(error => console.log(error.massage))
-
-//check input name//
-
-async function checkName() {
-
-    if(inputUserName.value.trim() === "") {
-        inputUserName.classList.add("red");
-    }
-    else {
-        inputUserName.classList.remove("red");
-    }
-}
-
-// //check input phone//
-
-async function checkPhone() {
-
-    if(inputUserPhone.value.trim() === "" || !(inputUserPhone.value.trim().includes("+")) || inputUserPhone.value.trim().split('').length < 12) {
-        inputUserPhone.classList.add("red");
-    }
-    else {
-        inputUserPhone.classList.remove("red");
-    }
-}
-
-//succes feedback//
-
-async function succes() {
-        let str = `
-                <h2 class="title_feedback">See you soon!</h2>
-                <p class="text_feedback">Ваші дані були успішно відправлені.
-                Будь ласка, очікуйте: я зв'яжуся з Вами якнайшвидше для обговорення деталей.</p>`;
-
-        contentForm.innerHTML = str;
-        const titleFdbk = document.querySelector(".title_feedback");
-        titleFdbk.style.fontSize = "80px";
-        titleFdbk.style.margin = "0";
-        titleFdbk.style.textAlign = "center";
-}
-
-//succes error//
-
-async function errorr() {
-        let str = `
-                <h2 class="title_feedback">Error</h2>
-                <p class="text_feedback">На жаль, на сайті сталася помилка і Ваші дані не були відправлені. Спробуйте, будь ласка, пізніше.</p>`;
-
-        contentForm.innerHTML = str;
-        const titleFdbk = document.querySelector(".title_feedback");
-        titleFdbk.style.fontSize = "80px";
-        titleFdbk.style.margin = "0";
-        titleFdbk.style.textAlign = "center";
-} 
-
-buttonClick.addEventListener("click", function () {
-    modalWindow.style.display = "block";
-});
-
-butClose.addEventListener("click", function () {
-    modalWindow.style.display = "none";
-});
-
-inputUserName.addEventListener("input", (e) => {
-    checkName();
-});
-
-inputUserPhone.addEventListener("input", (e) => {
-    checkPhone();
-});
-
-butSubmit.addEventListener("click", (e) => {
-    checkName();
-    checkPhone();
-})
 
 formUser.addEventListener("submit", async (e) => {
     e.preventDefault();
@@ -129,3 +48,73 @@ formUser.addEventListener("submit", async (e) => {
         `;
         sendMessage(message);
 });
+
+
+//check input name//
+
+async function checkName() {
+
+    if(inputUserName.value.trim() === "") {
+        inputUserName.classList.add("red");
+    }
+    else {
+        inputUserName.classList.remove("red");
+    }
+}
+
+// //check input phone//
+
+async function checkPhone() {
+
+    if(inputUserPhone.value.trim() === "" || !(inputUserPhone.value.trim().includes("+")) || inputUserPhone.value.trim().split('').length < 12) {
+        inputUserPhone.classList.add("red");
+    }
+    else {
+        inputUserPhone.classList.remove("red");
+    }
+}
+
+//succes feedback//
+
+async function succes() {
+        let str = `
+                <h2 class="title_feedback">See you soon!</h2>
+                <p class="text_feedback">Ваші дані були успішно відправлені. Будь ласка, очікуйте: я зв'яжуся з Вами якнайшвидше для обговорення деталей.</p>`;
+
+        contentForm.innerHTML = str;
+        contentForm.style.flexDirection = "column";
+        contentForm.style.gap = "24px";
+}
+
+//succes error//
+
+async function errorr() {
+        let str = `
+                <h2 class="title_feedback">Error</h2>
+                <p class="text_feedback">На жаль, на сайті сталася помилка і Ваші дані не<br>були відправлені. Спробуйте, будь ласка, пізніше.</p>`;
+
+        contentForm.innerHTML = str;
+        contentForm.style.flexDirection = "column";
+        contentForm.style.gap = "24px";
+}
+
+buttonClick.addEventListener("click", function () {
+    modalWindow.style.display = "block";
+});
+
+butClose.addEventListener("click", function () {
+    modalWindow.style.display = "none";
+});
+
+inputUserName.addEventListener("input", (e) => {
+    checkName();
+});
+
+inputUserPhone.addEventListener("input", (e) => {
+    checkPhone();
+});
+
+butSubmit.addEventListener("click", (e) => {
+    checkName();
+    checkPhone();
+})
